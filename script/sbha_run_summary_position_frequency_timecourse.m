48406f3e08c2451b2ef951c420791bb1f99d4d1c
nfagan <nick@fagan.org>
2019-11-13 14:38:17 -0500

    add meta information

--- script/sbha_run_summary_position_frequency_timecourse.m
+++ script/sbha_run_summary_position_frequency_timecourse.m
@@ -10,6 +10,9 @@ xls_save_dir = fullfile( sbha.dataroot(), 'xls', 'position_frequency_timecourse_
 % Whether to use excel trial criteria
 use_trial_selection_criterion = false;
 
+% Whether to use only correct, selected trials.
+use_only_correct_selected_trials = false;
+
 % Restrict input files to those containing string(s). Leave empty: {} to
 % include all files.
 files_containing = {'nc-congruent-twotarg-03-Dec-2018 17_54_41'};
@@ -36,7 +39,12 @@ outs = sbha_run_binned_position_frequency_timecourse( ...
 
 labels = outs.labels';
 
-mask = make_mask( labels, use_trial_selection_criterion );
+if ( use_only_correct_selected_trials )
+  mask = make_correct_selected_mask( labels, use_trial_selection_criterion );
+else
+  mask = make_all_mask( labels, use_trial_selection_criterion );
+end
+
 %%
 
 roi_left = get_roi( outs.norm_roi_left, stat_position_padding );
@@ -57,9 +65,24 @@ end
 
 function do_save_xls(counts, labels, xls_save_dir)
 
+assert_ispair( counts, labels );
+
+meta_labs = make_additional_meta_labels( labels );
+
+all_data = cell( rows(labels), size(counts, 2) + size(meta_labs, 2) );
+all_data(:, 1:size(counts, 2)) = arrayfun( @(x) x, counts, 'un', 0 );
+all_data(:, (size(counts, 2)+1):end) = meta_labs;
+
 shared_utils.io.require_dir( xls_save_dir );
 save_p = fullfile( xls_save_dir, xls_filename(labels) );
 xlswrite( save_p, counts );
+
+end
+
+function labs = make_additional_meta_labels(labels)
+
+labs = cellstr( labels, {'correct', 'made-selection' ...
+  , 'correct-direction', 'selected-direction', 'subject', 'identifier'} );
 
 end
 
@@ -80,7 +103,7 @@ norm_roi(:, 2) = norm_roi(:, 2) + padding/2;
 
 end
 
-function mask = make_mask(labels, use_trial_selection_criterion)
+function mask = make_all_mask(labels, use_trial_selection_criterion)
 
 if ( use_trial_selection_criterion )
   % Only trials that were selected
@@ -91,7 +114,23 @@ else
 end
 
 mask = fcat.mask( labels, mask ...
-  , @find, {'made-selection-true', 'collapsed-cue-direction-true'} ...
+  , @find, {'collapsed-cue-direction-false'} ...
+);
+
+end
+
+function mask = make_correct_selected_mask(labels, use_trial_selection_criterion)
+
+if ( use_trial_selection_criterion )
+  % Only trials that were selected
+  mask = find( labels, 'rt-is-trial-selected-true' );
+else
+  % All trials
+  mask = rowmask( labels );
+end
+
+mask = fcat.mask( labels, mask ...
+  , @find, {'made-selection-true', 'collapsed-cue-direction-false'} ...
   , @find, {'correct-true'} ...
 );
 
